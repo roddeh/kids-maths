044e67f335917ef7e6281a67fb2e576f68fb5a89
simon <simon@roddeh.com>
2018-08-14 07:27:20 -0700

    removed reference to local lib roddeh-rand, instead use the project rand.js

--- js/utils/shuffle.js
+++ js/utils/shuffle.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const rand = require('roddeh-rand');
+const rand = require('./rand')
 
 const shuffle = function(arr){
-  arr = arr.slice();
-  let shuffled = [];
+  arr = arr.slice()
+  let shuffled = []
   while(arr.length){
-    shuffled.push(arr.splice(rand(0, arr.length - 1), 1)[0]);
+    shuffled.push(arr.splice(rand(0, arr.length - 1), 1)[0])
   }
-  return shuffled;
+  return shuffled
 }
 
-export default shuffle;
+export default shuffle
